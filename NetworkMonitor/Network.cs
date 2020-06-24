4b2296442a49f6687a451e9b951eb10c2dd3bc97
RobertsMaxwell <max031029@gmail.com>
2020-06-24 15:29:43 -0600

    Displays processes read/write speed in bytes in CLI

---
+++ NetworkMonitor/Network.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NetworkMonitor
+{
+    class Network
+    {
+        static List<string> processNames = new List<string>();
+        static List<string[]> netStatsResults = new List<string[]>();
+        static List<Thread> threads = new List<Thread>();
+
+        public static string[][] Usage()
+        {
+            foreach (Process proc in Process.GetProcesses())
+            {
+                try
+                {
+                    if (!processNames.Contains(proc.ProcessName))
+                    {
+                        processNames.Add(proc.ProcessName);
+                    }
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("!\n" + e.Message + "\n!");
+                }
+            }
+
+            foreach (string name in processNames)
+            {
+                var thread = new Thread(delegate() { PopulateResult(name); });
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                while (thread.IsAlive)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+
+            return netStatsResults.ToArray();
+        }
+
+        public static void PopulateResult(string name)
+        {
+            try
+            {
+                var result = ProcessNetworkStats.GetBytePerSecond(name);
+                netStatsResults.Add(result);
+            }
+            catch (Exception) { }
+            
+            Thread.CurrentThread.Abort();
+        }
+    }
+}
---
+++ NetworkMonitor/ProcessNetworkStats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NetworkMonitor
+{
+    static class ProcessNetworkStats
+    {
+
+        //returns the one second difference between read and write bytes, respectively, in an array
+        public static string[] GetBytePerSecond(string process)
+        {
+            var firstRead = new PerformanceCounter("Process", "IO Read Bytes/sec", process).RawValue;
+            var firstWrite = new PerformanceCounter("Process", "IO Write Bytes/sec", process).RawValue;
+            Thread.Sleep(1000);
+            var secondRead = new PerformanceCounter("Process", "IO Read Bytes/sec", process).RawValue;
+            var secondWrite = new PerformanceCounter("Process", "IO Write Bytes/sec", process).RawValue;
+
+            return new string[] { Convert.ToString(secondRead - firstRead), Convert.ToString(secondWrite - firstWrite), process };
+        }
+    }
+}
